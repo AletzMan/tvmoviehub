9acc2f6385894e47942993ce0acca1e2c3402daf
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-06-25 19:49:24 -0600

    Chore: add svg components and helper functions

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -1,19 +1,21 @@
-/*import 'server-only'
- 
-const dictionaries = {
-  en: () => import('../../public/locale/en.json').then((module) => module.default),
-  es: () => import('../../public/locale/es.json').then((module) => module.default),
-}
- 
-export const getDictionary = async (locale: string) => dictionaries[locale]()*/
+import { SmallDateLocal } from "./const"
 
-type Dictionary = Record<string, string>
-
-const dictionaries: Record<string, () => Promise<Dictionary>> = {
-	en: () => import("../../public/locale/en.json").then((module) => module.default),
-	es: () => import("../../public/locale/es.json").then((module) => module.default),
+export const ConvertMinutesToHours = (time: number) => {
+	const hours = Math.floor(time / 60)
+	const minutes = time - hours * 60
+	console.log(hours, minutes)
+	return `${hours}h ${minutes}m`
 }
 
-export const getDictionary = async (locale: keyof typeof dictionaries): Promise<Dictionary> => {
-	return dictionaries[locale]()
+export const FormattedDate = (date: string, type: "small" | "long") => {
+	const newDate = date.split("-")
+	const day = Number(newDate[2]) + 1
+	const month = newDate[1]
+	const year = newDate[0]
+	const birthday = new Date(date).getTime()
+	const currentTime = new Date().getTime()
+	const age = currentTime - birthday
+	const formatted = new Date(`${year}-${month}-${day}`).toLocaleDateString("es-MX", SmallDateLocal)
+	const years = Math.floor(((((age / 1000) / 60) / 60) / 24) / 365)
+	return `${formatted} (${years} años)`
 }
