9ca6d2b6d6677e9dd041dba173ca9c35bc9c0070
= <=>
2024-06-25 19:49:24 -0600

    Chore: add svg components and helper functions

--- app/utils/svg.tsx
+++ app/utils/svg.tsx
@@ -34,6 +34,14 @@ export function MovieListIcon({ className }: Props) {
     )
 }
 
+export function PeopleIcon({ className }: Props) {
+    return (
+        <svg className={className} width="1em" height="1em" viewBox="0 0 16 16">
+            <path fill="currentColor" d="M15 14s1 0 1-1s-1-4-5-4s-5 3-5 4s1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276c.593.69.758 1.457.76 1.72l-.008.002l-.014.002zM11 7a2 2 0 1 0 0-4a2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0a3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904c.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724c.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0a3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4a2 2 0 0 0 0-4" />
+        </svg>
+    )
+}
+
 export function SettingsIcon({ className }: Props) {
     return (
         <svg className={className} width="1em" height="1em" viewBox="-2 -4 24 24">
@@ -76,5 +84,73 @@ export function SerieIcon({ className }: Props) {
                 <path d="M 128,64 80,16" />
             </g>
         </svg>
+    )
+}
+
+export function ArrowLeftIcon({ className }: Props) {
+    return (
+        <svg className={className} width="1em" height="1em" viewBox="0 0 24 24">
+            <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m15 5l-6 7l6 7" />
+        </svg>
+    )
+}
+
+export function SearchIcon({ className }: Props) {
+    return (
+        <svg className={className} width="1em" height="1em" viewBox="0 0 24 24">
+            <path fill="currentColor" fillRule="evenodd" d="M14.385 15.446a6.75 6.75 0 1 1 1.06-1.06l5.156 5.155a.75.75 0 1 1-1.06 1.06zm-7.926-1.562a5.25 5.25 0 1 1 7.43-.005l-.005.005l-.005.004a5.25 5.25 0 0 1-7.42-.004" clipRule="evenodd" />
+        </svg>
+    )
+}
+
+export function StarIcon({ className }: Props) {
+    return (
+        <svg className={className} width="1em" height="1em" viewBox="0 0 24 24">
+            <path fill="currentColor" d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08z" />
+        </svg>
+    )
+}
+
+export function AddIcon({ className }: Props) {
+    return (
+        <svg className={className} width="1em" height="1em" viewBox="0 0 32 32">
+            <path fill="currentColor" d="M7.5 3A4.5 4.5 0 0 0 3 7.5v17A4.5 4.5 0 0 0 7.5 29h17a4.5 4.5 0 0 0 4.5-4.5v-17A4.5 4.5 0 0 0 24.5 3zM16 8a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2h-6v6a1 1 0 1 1-2 0v-6H9a1 1 0 1 1 0-2h6V9a1 1 0 0 1 1-1" />
+        </svg>
+    )
+}
+
+export function DateIcon({ className }: Props) {
+    return (
+        <svg className={className} width="1em" height="1em" viewBox="0 0 20 20">
+            <path fill="currentColor" d="M5.673 0a.7.7 0 0 1 .7.7v1.309h7.517v-1.3a.7.7 0 0 1 1.4 0v1.3H18a2 2 0 0 1 2 1.999v13.993A2 2 0 0 1 18 20H2a2 2 0 0 1-2-1.999V4.008a2 2 0 0 1 2-1.999h2.973V.699a.7.7 0 0 1 .7-.699M1.4 7.742v10.259a.6.6 0 0 0 .6.6h16a.6.6 0 0 0 .6-.6V7.756zm5.267 6.877v1.666H5v-1.666zm4.166 0v1.666H9.167v-1.666zm4.167 0v1.666h-1.667v-1.666zm-8.333-3.977v1.666H5v-1.666zm4.166 0v1.666H9.167v-1.666zm4.167 0v1.666h-1.667v-1.666zM4.973 3.408H2a.6.6 0 0 0-.6.6v2.335l17.2.014V4.008a.6.6 0 0 0-.6-.6h-2.71v.929a.7.7 0 0 1-1.4 0v-.929H6.373v.92a.7.7 0 0 1-1.4 0z" />
+        </svg>
+    )
+}
+
+export function TimeIcon({ className }: Props) {
+    return (
+        <svg className={className} width="1em" height="1em" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2M12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8m-.22-13h-.06c-.4 0-.72.32-.72.72v4.72c0 .35.18.68.49.86l4.15 2.49c.34.2.78.1.98-.24a.71.71 0 0 0-.25-.99l-3.87-2.3V7.72c0-.4-.32-.72-.72-.72" />
+        </svg>
+    )
+}
+
+export function TrilerIcon({ className }: Props) {
+    return (
+        <svg className={className} width="1em" height="1em" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M20 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2m.001 6c-.001 0-.001 0 0 0h-.465l-2.667-4H20zM9.536 9L6.869 5h2.596l2.667 4zm5 0l-2.667-4h2.596l2.667 4zM4 5h.465l2.667 4H4zm0 14v-8h16l.002 8z" />
+            <path fill="currentColor" d="m10 18l5.5-3l-5.5-3z" />
+        </svg>
+    )
+}
+
+export function LoadingIcon({ className }: Props) {
+    return (
+        <svg className={className} width="1em" height="1em" viewBox="0 0 24 24">
+            <g fill="none" fillRule="evenodd">
+                <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                <path fill="currentColor" d="M12 4.5a7.5 7.5 0 1 0 0 15a7.5 7.5 0 0 0 0-15M1.5 12C1.5 6.201 6.201 1.5 12 1.5S22.5 6.201 22.5 12S17.799 22.5 12 22.5S1.5 17.799 1.5 12" opacity="0.1" />
+                <path fill="currentColor" d="M12 4.5a7.458 7.458 0 0 0-5.187 2.083a1.5 1.5 0 0 1-2.075-2.166A10.458 10.458 0 0 1 12 1.5a1.5 1.5 0 0 1 0 3" />
+            </g></svg>
     )
 }
