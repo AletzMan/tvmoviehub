97b80cfafee5312c4e6f815292dbbd67d0afc816
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-06-25 19:44:34 -0600

    Add libraries adn config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ['image.tmdb.org', 'raw.githubusercontent.com'],
+    },
 }
 
 export default nextConfig
