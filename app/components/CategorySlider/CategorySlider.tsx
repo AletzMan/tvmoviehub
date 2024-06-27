a11f454ce9375081c537908ba218beccd78af576
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-06-27 00:18:34 -0600

    Chore: add category slider component

---
+++ app/components/CategorySlider/CategorySlider.tsx
@@ -0,0 +1,32 @@
+import { CategoriesMovies, CategoriesSeries } from "@/app/utils/const"
+import styles from "./category.module.scss"
+import { MovieSliderGeneral } from "../MovieSlider/MovieSliderGeneral"
+import Link from "next/link"
+
+interface Props {
+    type: 'serie' | 'movie'
+}
+
+export const CategorySlider = ({ type }: Props) => {
+
+    return (
+        <MovieSliderGeneral title="Categorías"  >
+            {type === "movie" && CategoriesMovies.map(category => (
+                <div className={styles.category} key={category.id}>
+                    <Link className={styles.category_link} href={`movies/category/${category.link}`}>
+                        {category.icon}
+                        <span className={styles.category_name}>{category.name}</span>
+                    </Link>
+                </div>
+            ))}
+            {type === "serie" && CategoriesSeries.map(category => (
+                <div className={styles.category} key={category.id}>
+                    <Link className={styles.category_link} href={`series/category/${category.link}`}>
+                        {category.icon}
+                        <span className={styles.category_name}>{category.name}</span>
+                    </Link>
+                </div>
+            ))}
+        </MovieSliderGeneral>
+    )
+}
