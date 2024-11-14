5e55898348efea8ca16dddb187844a79fc6e93f9
= <=>
2024-11-13 23:24:12 -0600

    Chore: remove console

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,5 +1,4 @@
 import { HeaderSection } from "../components/HeaderSection/HeaderSection"
-import { IQueryParamasMovies } from "../interfaces/movie"
 import { FavoriteFullIcon } from "../utils/svg"
 import FavoriteMovies from "./components/FavoriteMovies/FavoriteMovies"
 import FavoriteSeries from "./components/FavoriteSeries/FavoriteSeries"
@@ -7,7 +6,6 @@ import SelectFavorite from "./components/SelectFavorite/SelectFavorite"
 import styles from "./styles.module.scss"
 
 export default async function Page(params: { searchParams: { type: string, page: string } }) {
-    console.log(params.searchParams)
     return (
         <section>
             <HeaderSection title="Mis favoritos" icon={<FavoriteFullIcon />} />
