8a2a5f1b180585c76264f57208f3eac9f49c7cd3
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-11-20 22:47:43 -0600

    Chore: add loading page

--- app/components/LoadingPage/LoadingPage.tsx
+++ app/components/LoadingPage/LoadingPage.tsx
@@ -1,14 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client"
 import { LoadingIcon } from "@/app/utils/svg"
 import styles from "./styles.module.scss"
+import { usePathname } from "next/navigation"
+import { useEffect } from "react"
+import { useLoadingState } from "@/app/services/store"
 
 export function LoadingPage() {
+    const { loadingState, setLoadingState } = useLoadingState()
+    const pathname = usePathname()
+
+    useEffect(() => {
+        if (loadingState)
+            setLoadingState(false)
+    }, [pathname])
 
     return (
-        <dialog open className={styles.dialog}>
-            <div className={styles.dialog_container}>
-                <LoadingIcon className={styles.dialog_icon} />
-                <span className={styles.dialog_text}>CARGANDO</span>
-            </div>
-        </dialog>
+        <>
+            {loadingState &&
+                <dialog open className={styles.dialog}>
+                    <div className={styles.dialog_container}>
+                        <LoadingIcon className={styles.dialog_icon} />
+                        <span className={styles.dialog_text}>CARGANDO</span>
+                    </div>
+                </dialog>
+            }
+        </>
     )
 }
--- app/components/SideMenu/SideMenu.tsx
+++ app/components/SideMenu/SideMenu.tsx
@@ -4,10 +4,12 @@ import styles from "./sidemenu.module.scss"
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
 import { useSession } from "@/app/hooks/useSession"
+import { useLoadingState } from "@/app/services/store"
 
 export const SideMenu = () => {
     const session = useSession()
     const pathname = usePathname()
+    const { loadingState, setLoadingState } = useLoadingState()
     const section = pathname.split("/")[1]
     const router = useRouter()
 
@@ -20,40 +22,43 @@ export const SideMenu = () => {
         }
     }
 
+
     return (
-        <aside className={`${styles.aside} scrollBarStyle`}>
-            <nav className={styles.menu}>
-                <div className={styles.menu_section}>
-                    {<h2 className={styles.menu_title}>Menu</h2>}
-                    {
-                        MainMenu.filter((_, index) => index < 5 && index > 0).map((menu, index) => (
-                            <Link key={menu.id} className={`${styles.menu_item} ${menu.link === section && styles.menu_itemCurrent} ${menu.name}`} href={`/${menu.link}${index > 0 ? "?page=1" : ""}`} title={`Ir a ${menu.name}`}>{menu.icon}{menu.name}</Link>
-                        ))
+        <>
+            <aside className={`${styles.aside} scrollBarStyle`}>
+                <nav className={styles.menu}>
+                    <div className={styles.menu_section}>
+                        {<h2 className={styles.menu_title}>Menu</h2>}
+                        {
+                            MainMenu.filter((_, index) => index < 5 && index > 0).map((menu, index) => (
+                                <Link key={menu.id} className={`${styles.menu_item} ${menu.link === section && styles.menu_itemCurrent} ${menu.name}`} onClick={() => setLoadingState(true)} href={`/${menu.link}${index > 0 ? "?page=1" : ""}`} title={`Ir a ${menu.name}`}>{menu.icon}{menu.name}</Link>
+                            ))
+                        }
+                    </div>
+                    {session.session_id &&
+                        <>
+                            <div className={styles.separator}></div>
+                            <div className={styles.menu_section}>
+                                {<h2 className={styles.menu_title}>Usuario</h2>}
+                                {
+                                    MainMenu.filter((_, index) => index > 4 && index < 8).map(menu => (
+                                        <Link key={menu.id} className={`${styles.menu_item} ${menu.link.split("?")[0] === section && styles.menu_itemCurrent}  ${menu.name}`} onClick={() => setLoadingState(true)} href={`/${menu.link}`} title={`Ir a ${menu.name}`}>{menu.icon}{menu.name}</Link>
+                                    ))
+                                }
+                            </div>
+                        </>
                     }
-                </div>
-                {session.session_id &&
-                    <>
-                        <div className={styles.separator}></div>
-                        <div className={styles.menu_section}>
-                            {<h2 className={styles.menu_title}>Usuario</h2>}
-                            {
-                                MainMenu.filter((_, index) => index > 4 && index < 8).map(menu => (
-                                    <Link key={menu.id} className={`${styles.menu_item} ${menu.link.split("?")[0] === section && styles.menu_itemCurrent}  ${menu.name}`} href={`/${menu.link}`} title={`Ir a ${menu.name}`}>{menu.icon}{menu.name}</Link>
-                                ))
-                            }
-                        </div>
-                    </>
-                }
-                {<div className={`${styles.separator} ${styles.separator_general}`}></div>}
-                {<div className={`${styles.menu_section} ${styles.menu_sectionGeneral}`}>
-                    {<h2 className={styles.menu_title}>General</h2>}
-                    {session.session_id ?
-                        <button className={`${styles.menu_item} ${MainMenu[9].link === section && styles.menu_itemCurrent}  ${MainMenu[9].name}`} title={`Ir a ${MainMenu[9].name}`} onClick={HandleSession}>{MainMenu[9].icon}{MainMenu[9].name}</button>
-                        :
-                        <Link className={`${styles.menu_item} ${MainMenu[10].link === section && styles.menu_itemCurrent}  ${MainMenu[10].name}`} href={`/${MainMenu[10].link}`} title={`Ir a ${MainMenu[10].name}`}>{MainMenu[10].icon}{MainMenu[10].name}</Link>
-                    }
-                </div>}
-            </nav>
-        </aside>
+                    {<div className={`${styles.separator} ${styles.separator_general}`}></div>}
+                    {<div className={`${styles.menu_section} ${styles.menu_sectionGeneral}`}>
+                        {<h2 className={styles.menu_title}>General</h2>}
+                        {session.session_id ?
+                            <button className={`${styles.menu_item} ${MainMenu[9].link === section && styles.menu_itemCurrent}  ${MainMenu[9].name}`} title={`Ir a ${MainMenu[9].name}`} onClick={HandleSession}>{MainMenu[9].icon}{MainMenu[9].name}</button>
+                            :
+                            <Link className={`${styles.menu_item} ${MainMenu[10].link === section && styles.menu_itemCurrent}  ${MainMenu[10].name}`} href={`/${MainMenu[10].link}`} title={`Ir a ${MainMenu[10].name}`}>{MainMenu[10].icon}{MainMenu[10].name}</Link>
+                        }
+                    </div>}
+                </nav>
+            </aside>
+        </>
     )
 }
