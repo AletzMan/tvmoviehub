ba49f21a1ed20b5db54a9fb49e246a5b1c09bb77
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-11-22 00:58:21 -0600

    Chore: add externalid interface

--- app/interfaces/movie.ts
+++ app/interfaces/movie.ts
@@ -165,3 +165,12 @@ export interface IMovieStates {
 	reted: boolean
 	watchlist: boolean
 }
+
+export interface IExternalIDs {
+	id: number
+	imdb_id: string
+	wikidata_id: string
+	facebook_id: string
+	instagram_id: string
+	twitter_id: string
+}
