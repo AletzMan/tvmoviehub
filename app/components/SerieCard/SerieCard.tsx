d7946cf1805f7ee988c550081cee085ae66a32c6
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-12-05 16:01:53 -0600

    Update: change styles

--- app/components/SerieCard/SerieCard.tsx
+++ app/components/SerieCard/SerieCard.tsx
@@ -7,6 +7,8 @@ import { BASE_URL_IMG } from "@/app/utils/const"
 import { ISerie } from "@/app/interfaces/serie"
 import { FavoriteButton } from "../FavoriteButton/FavoriteButton"
 import { useLoadingState } from "@/app/services/store"
+import { MediaOptions } from "../MediaOptions/MediaOptions"
+import { useState } from "react"
 
 interface Props {
     serie: ISerie
@@ -15,13 +17,16 @@ interface Props {
 
 export const SerieCard = ({ serie, top }: Props) => {
     const { setLoadingState } = useLoadingState()
+    const [viewMenu, setViewMenu] = useState(false)
     return (
         <div key={serie.id} className={styles.movie}>
+            <div className={styles.movie_frame}></div>
             <Image className={styles.movie_backdrop} src={serie.poster_path ? BASE_URL_IMG.concat(serie.backdrop_path || '') : "/not_photo.png"} width={150} height={230} alt={`Poster de ${serie.name}`} />
             <div className={styles.movie_picture}>
-                <Image className={styles.movie_photo} src={serie.poster_path ? BASE_URL_IMG.concat(serie.poster_path || '') : "/not_photo.png"} width={150} height={230} alt={`Poster de ${serie.name}`} />
+                <Image className={styles.movie_photo} src={serie.poster_path ? BASE_URL_IMG.concat(serie.poster_path || '') : "/not_photo.png"} width={160} height={230} alt={`Poster de ${serie.name}`} />
             </div>
             <div className={styles.movie_shadow}></div>
+            <div className={`${styles.movie_shadowTop} ${viewMenu ? styles.movie_shadowBlur : ""}`}></div>
             <div className={styles.movie_dialog}>
                 <Link className={styles.movie_dialogMore} href={`/series/${serie.id}`} title={serie.name} onClick={() => setLoadingState(true)}>
                     <DetailsIcon className={styles.movie_dialogIcon} />
@@ -29,7 +34,7 @@ export const SerieCard = ({ serie, top }: Props) => {
             </div>
             {<span className={styles.movie_number}>{top}</span>}
             <span className={styles.movie_average}><StarIcon className={styles.movie_iconDate} />{serie.vote_average.toFixed(1)}</span>
-            <FavoriteButton id={serie.id} title={serie.name} type="tv" />
+            <MediaOptions id={serie.id} title={serie.name} type="tv" setViewMenu={setViewMenu} viewMenu={viewMenu} />
             <div className={styles.movie_description}>
                 {/*<span className={styles.movie_type}><MovieIcon className={styles.movie_typeIcon} />Película</span>*/}
 
