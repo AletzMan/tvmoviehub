debb333ab0dedf675cc9e8cd5e866683cf1c16ad
BitCoder <alejo_2986@hotmail.com>
2026-08-09 20:19:00 -0600

    Improve MediaCard rankBadge with custom SVG ribbon design

--- app/components/MediaCard/MediaCard.tsx
+++ app/components/MediaCard/MediaCard.tsx
@@ -88,7 +88,18 @@ export const MediaCard = ({
             onPointerMove={handlePointerMove}
             onClick={handleCardClick}
         >
-            {showRank && <span className={styles.mediaCard_rankBadge}>#{showRank}</span>}
+            {showRank &&
+                <div className={styles.mediaCard_rankBadge}>
+                    <svg width="60"
+                        height="40"
+                        viewBox="95 85 62 45"
+                        className={styles.mediaCard_svg}>
+                        <path d="m 155.69565,88.066944 -7.17837,0.0155 h -42.56949 c -5.97531,-2e-6 -10.819497,4.844179 -10.819498,10.819492 v 25.735904 l -0.01395,3.96255 c 0.359393,-1.88082 2.148328,-3.74066 4.234368,-3.96255 h 34.63716 c 8.02567,0 14.53142,-6.50626 14.53142,-14.53193 V 98.411016 c 0,-4.577005 3.2152,-8.860428 7.17837,-10.344075 z"></path>
+                    </svg>
+                    <span>#{showRank}</span>
+                </div>
+            }
+
 
             <div className={styles.mediaCard_posterWrapper}>
                 <Image
