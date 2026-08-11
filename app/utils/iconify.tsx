c400b504f99afcee06f4ad0b8435ffa0d14cbb44
BitCoder <alejo_2986@hotmail.com>
2026-08-11 14:51:05 -0600

    Refactor code structure for improved readability and maintainability

---
+++ app/utils/iconify.tsx
@@ -0,0 +1,323 @@
+import { Icon } from '@iconify/react'
+
+interface Props {
+    className?: string
+}
+
+// Iconos de UI comunes
+export function FavoriteEmptyIcon({ className }: Props) {
+    return <Icon icon="mdi:heart-outline" className={className} />
+}
+
+export function FavoriteFullIcon({ className }: Props) {
+    return <Icon icon="mdi:heart" className={className} />
+}
+
+export function BookmarkIcon({ className }: Props) {
+    return <Icon icon="mdi:bookmark" className={className} />
+}
+
+export function StarIcon({ className }: Props) {
+    return <Icon icon="mdi:star" className={className} />
+}
+
+export function OptionsIcon({ className }: Props) {
+    return <Icon icon="mdi:dots-vertical" className={className} />
+}
+
+export function SearchIcon({ className }: Props) {
+    return <Icon icon="mdi:magnify" className={className} />
+}
+
+export function HomeIcon({ className }: Props) {
+    return <Icon icon="mdi:home" className={className} />
+}
+
+export function AccountIcon({ className }: Props) {
+    return <Icon icon="mdi:account" className={className} />
+}
+
+export function LogoutIcon({ className }: Props) {
+    return <Icon icon="mdi:logout" className={className} />
+}
+
+export function DeleteIcon({ className }: Props) {
+    return <Icon icon="mdi:delete" className={className} />
+}
+
+export function ArrowLeftIcon({ className }: Props) {
+    return <Icon icon="mdi:arrow-left" className={className} />
+}
+
+export function ArrowRightIcon({ className }: Props) {
+    return <Icon icon="mdi:arrow-right" className={className} />
+}
+
+export function PlayIcon({ className }: Props) {
+    return <Icon icon="mdi:play" className={className} />
+}
+
+export function CloseIcon({ className }: Props) {
+    return <Icon icon="mdi:close" className={className} />
+}
+
+export function MenuIcon({ className }: Props) {
+    return <Icon icon="mdi:menu" className={className} />
+}
+
+export function LogInIcon({ className }: Props) {
+    return <Icon icon="mdi:login" className={className} />
+}
+
+export function AddIcon({ className }: Props) {
+    return <Icon icon="mdi:plus-box" className={className} />
+}
+
+export function CheckOnlyIcon({ className }: Props) {
+    return <Icon icon="mdi:check" className={className} />
+}
+
+export function ListIcon({ className }: Props) {
+    return <Icon icon="mdi:format-list-bulleted" className={className} />
+}
+
+export function SettingsIcon({ className }: Props) {
+    return <Icon icon="mdi:cog" className={className} />
+}
+
+export function PeopleIcon({ className }: Props) {
+    return <Icon icon="mdi:account-group" className={className} />
+}
+
+export function MovieIcon({ className }: Props) {
+    return <Icon icon="mdi:movie" className={className} />
+}
+
+export function SerieIcon({ className }: Props) {
+    return <Icon icon="mdi:television" className={className} />
+}
+
+export function ImageIcon({ className }: Props) {
+    return <Icon icon="mdi:image" className={className} />
+}
+
+export function FacebookIcon({ className }: Props) {
+    return <Icon icon="mdi:facebook" className={className} />
+}
+
+export function XIcon({ className }: Props) {
+    return <Icon icon="mdi:x-twitter" className={className} />
+}
+
+export function InstagramIcon({ className }: Props) {
+    return <Icon icon="mdi:instagram" className={className} />
+}
+
+export function SaveIcon({ className }: Props) {
+    return <Icon icon="mdi:content-save" className={className} />
+}
+
+export function DateIcon({ className }: Props) {
+    return <Icon icon="mdi:calendar" className={className} />
+}
+
+export function TimeIcon({ className }: Props) {
+    return <Icon icon="mdi:clock" className={className} />
+}
+
+export function ViewIcon({ className }: Props) {
+    return <Icon icon="mdi:eye" className={className} />
+}
+
+export function ViewOffIcon({ className }: Props) {
+    return <Icon icon="mdi:eye-off" className={className} />
+}
+
+export function ArrowDownIcon({ className }: Props) {
+    return <Icon icon="mdi:arrow-down" className={className} />
+}
+
+export function ArrowDownSolidIcon({ className }: Props) {
+    return <Icon icon="mdi:arrow-down-bold" className={className} />
+}
+
+export function NextPageIcon({ className }: Props) {
+    return <Icon icon="mdi:chevron-right" className={className} />
+}
+
+export function PrevPageIcon({ className }: Props) {
+    return <Icon icon="mdi:chevron-left" className={className} />
+}
+
+export function SortDownIcon({ className }: Props) {
+    return <Icon icon="mdi:sort-descending" className={className} />
+}
+
+export function SortUpIcon({ className }: Props) {
+    return <Icon icon="mdi:sort-ascending" className={className} />
+}
+
+export function FiltersIcon({ className }: Props) {
+    return <Icon icon="mdi:filter" className={className} />
+}
+
+export function ResetIcon({ className }: Props) {
+    return <Icon icon="mdi:refresh" className={className} />
+}
+
+export function NotFoundIcon({ className }: Props) {
+    return <Icon icon="mdi:magnify-close" className={className} />
+}
+
+export function NotIcon({ className }: Props) {
+    return <Icon icon="mdi:block-helper" className={className} />
+}
+
+export function SuccessIcon({ className }: Props) {
+    return <Icon icon="mdi:check-circle" className={className} />
+}
+
+export function LoadingIcon({ className }: Props) {
+    return <Icon icon="mdi:loading" className={className} />
+}
+
+export function TrilerIcon({ className }: Props) {
+    return <Icon icon="mdi:play-circle" className={className} />
+}
+
+export function ClearIcon({ className }: Props) {
+    return <Icon icon="mdi:delete-sweep" className={className} />
+}
+
+export function MovieListIcon({ className }: Props) {
+    return <Icon icon="mdi:movie-open" className={className} />
+}
+
+export function MaleIcon({ className }: Props) {
+    return <Icon icon="mdi:gender-male" className={className} />
+}
+
+export function FemaleIcon({ className }: Props) {
+    return <Icon icon="mdi:gender-female" className={className} />
+}
+
+export function UpcomingIcon({ className }: Props) {
+    return <Icon icon="mdi:calendar-clock" className={className} />
+}
+
+export function TopIcon({ className }: Props) {
+    return <Icon icon="mdi:trending-up" className={className} />
+}
+
+export function PopularIcon({ className }: Props) {
+    return <Icon icon="mdi:star-face" className={className} />
+}
+
+export function ListAddIcon({ className }: Props) {
+    return <Icon icon="mdi:playlist-plus" className={className} />
+}
+
+export function FolderMovieIcon({ className }: Props) {
+    return <Icon icon="mdi:folder-movie" className={className} />
+}
+
+export function DetailsIcon({ className }: Props) {
+    return <Icon icon="mdi:information" className={className} />
+}
+
+// Iconos de géneros
+export function ActionIcon({ className }: Props) {
+    return <Icon icon="mdi:movie-open" className={className} />
+}
+
+export function AdventureIcon({ className }: Props) {
+    return <Icon icon="mdi:compass" className={className} />
+}
+
+export function AnimationIcon({ className }: Props) {
+    return <Icon icon="mdi:animation" className={className} />
+}
+
+export function ComedyIcon({ className }: Props) {
+    return <Icon icon="mdi:emoticon-happy" className={className} />
+}
+
+export function CrimeIcon({ className }: Props) {
+    return <Icon icon="mdi:pistol" className={className} />
+}
+
+export function DocumentaryIcon({ className }: Props) {
+    return <Icon icon="mdi:video-box" className={className} />
+}
+
+export function DramaIcon({ className }: Props) {
+    return <Icon icon="mdi:theater" className={className} />
+}
+
+export function FamilyIcon({ className }: Props) {
+    return <Icon icon="mdi:account-group" className={className} />
+}
+
+export function FantasyIcon({ className }: Props) {
+    return <Icon icon="mdi:auto-fix" className={className} />
+}
+
+export function HistoryIcon({ className }: Props) {
+    return <Icon icon="mdi:history" className={className} />
+}
+
+export function HorrorIcon({ className }: Props) {
+    return <Icon icon="mdi:ghost" className={className} />
+}
+
+export function MusicIcon({ className }: Props) {
+    return <Icon icon="mdi:music" className={className} />
+}
+
+export function MysteryIcon({ className }: Props) {
+    return <Icon icon="mdi:help-circle" className={className} />
+}
+
+export function RomanceIcon({ className }: Props) {
+    return <Icon icon="mdi:heart" className={className} />
+}
+
+export function SCIFiIcon({ className }: Props) {
+    return <Icon icon="mdi:rocket" className={className} />
+}
+
+export function TVMovieIcon({ className }: Props) {
+    return <Icon icon="mdi:television-classic" className={className} />
+}
+
+export function ThrillerIcon({ className }: Props) {
+    return <Icon icon="mdi:knife" className={className} />
+}
+
+export function WarIcon({ className }: Props) {
+    return <Icon icon="mdi:tank" className={className} />
+}
+
+export function WesternIcon({ className }: Props) {
+    return <Icon icon="mdi:horse" className={className} />
+}
+
+export function KidIcon({ className }: Props) {
+    return <Icon icon="mdi:face-man" className={className} />
+}
+
+export function NewsIcon({ className }: Props) {
+    return <Icon icon="mdi:newspaper" className={className} />
+}
+
+export function RealityIcon({ className }: Props) {
+    return <Icon icon="mdi:video" className={className} />
+}
+
+export function SoapIcon({ className }: Props) {
+    return <Icon icon="mdi:television" className={className} />
+}
+
+export function TalkIcon({ className }: Props) {
+    return <Icon icon="mdi:microphone" className={className} />
+}
