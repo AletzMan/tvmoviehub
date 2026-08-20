7aec431508b0431f2df414cf72f8817e11140b50
BitCoder <alejo_2986@hotmail.com>
2026-08-20 02:24:14 -0600

    refactor: rediseñar página de cuenta
    Updated account page with improved layout including decorative reel image, reorganized data sections with containers for movies/series stats, color-coded section icons, fixed profile image URL handling, and improved responsive styling.

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -6,7 +6,7 @@ import { AccountIcon, ArrowRightIcon, BookmarkIcon, FavoriteFullIcon, ListIcon, 
 import { GetDetailsAccount, GetFavoriteMovies, GetFavoriteSeries, GetLists, GetRated, GetWatchList } from "../services/fetchData"
 import { cookies } from "next/headers"
 import Image from "next/image"
-import { URL_IMAGE_NOTPHOTO } from "../utils/const"
+import { BASE_URL_IMG_CUSTOM, URL_IMAGE_NOTPHOTO_PROFILE } from "../utils/const"
 import styles from "./styles.module.scss"
 import Link from "next/link"
 
@@ -24,13 +24,13 @@ export default async function Page({ searchParams }: { searchParams: Promise<{ t
     const ratedSeries = await GetRated(cookie || "", params, "tv")
     const listMovies = await GetLists(cookie || "", { page: 1 })
 
-
     return (
         <section className={`${styles.section} scrollBarStyle`}>
             <HeaderSection title="Mi cuenta" icon={<AccountIcon />} />
             <div className={styles.account}>
+                <img className={styles.account_reel} src="https://raw.githubusercontent.com/AletzMan/ImagesStorage/refs/heads/main/film_data_hub/reel.png" />
                 <picture className={styles.account_picture}>
-                    <Image className={styles.account_photo} alt={`Foto de perfil de ${account?.username}`} overrideSrc={URL_IMAGE_NOTPHOTO} src={account?.avatar?.tmdb.avatar_path || URL_IMAGE_NOTPHOTO} width={115} height={165} />
+                    <Image className={styles.account_photo} alt={`Foto de perfil de ${account?.username}`} src={account?.avatar?.tmdb.avatar_path ? BASE_URL_IMG_CUSTOM.concat('/w300', account?.avatar?.tmdb.avatar_path) : URL_IMAGE_NOTPHOTO_PROFILE} width={115} height={165} />
                 </picture>
                 <div className={styles.account_info} >
                     <label className={styles.account_label}>ID:</label>
@@ -45,60 +45,75 @@ export default async function Page({ searchParams }: { searchParams: Promise<{ t
                 <div className={styles.data_section}>
                     <div className={styles.data_item}>
                         <Link className={styles.data_link} href={"/favorites?type=movies&page=1"}>
-                            <span className={styles.data_title}> <FavoriteFullIcon />Favoritos</span>
+                            <span className={styles.data_title}> <FavoriteFullIcon className={styles.data_logo} />Favoritos</span>
                             <ArrowRightIcon className={styles.data_icon} />
-
                         </Link>
-                        <span className={styles.data_label} >Peliculas</span>
-                        <span className={styles.data_count}>{favoritesMovies?.total_results}</span>
                     </div>
-                    <div className={styles.data_item}>
-                        <label className={styles.data_label}>Series de TV</label>
-                        <span className={styles.data_count}>{favoritesSeries?.total_results}</span>
+                    <div className={styles.data_container}>
+                        <div className={styles.data_item}>
+                            <span className={styles.data_label} >Peliculas</span>
+                            <span className={styles.data_count}>{favoritesMovies?.total_results}</span>
+                        </div>
+                        <div className={styles.data_item}>
+                            <label className={styles.data_label}>Series de TV</label>
+                            <span className={styles.data_count}>{favoritesSeries?.total_results}</span>
+                        </div>
                     </div>
                 </div>
                 <div className={styles.data_section}>
                     <div className={styles.data_item}>
-                        <Link className={styles.data_link} href={" /watchlist?type=movies&page=1"}>
+                        <Link className={styles.data_link} href={"/watchlist?type=movies&page=1"}>
                             <span className={styles.data_title}>
-                                <BookmarkIcon />  Lista de seguimiento
+                                <BookmarkIcon className={styles.data_logo} />  Lista de seguimiento
                             </span>
                             <ArrowRightIcon className={styles.data_icon} />
                         </Link>
-                        <label className={styles.data_label}>Peliculas</label>
-                        <span className={styles.data_count}>{watchListMovies?.total_results}</span>
                     </div>
-                    <div className={styles.data_item}>
-                        <label className={styles.data_label}>Series de TV</label>
-                        <span className={styles.data_count}>{watchListSeries?.total_results}</span>
+                    <div className={styles.data_container}>
+                        <div className={styles.data_item}>
+                            <span className={styles.data_label}>Peliculas</span>
+                            <span className={styles.data_count}>{watchListMovies?.total_results}</span>
+                        </div>
+                        <div className={styles.data_item}>
+                            <span className={styles.data_label}>Series de TV</span>
+                            <span className={styles.data_count}>{watchListSeries?.total_results}</span>
+                        </div>
                     </div>
                 </div>
                 <div className={styles.data_section}>
                     <div className={styles.data_item}>
-                        <Link className={styles.data_link} href={" /rated?type=movies&page=1"}>
+                        <Link className={styles.data_link} href={"/rated?type=movies&page=1"}>
                             <span className={styles.data_title}>
-                                <StarIcon />  Valoradas
+                                <StarIcon className={styles.data_logo} />  Valoradas
                             </span>
                             <ArrowRightIcon className={styles.data_icon} />
                         </Link>
-                        <label className={styles.data_label}>Peliculas</label>
-                        <span className={styles.data_count}>{ratedMovies?.total_results}</span>
                     </div>
-                    <div className={styles.data_item}>
-                        <label className={styles.data_label}>Series de TV</label>
-                        <span className={styles.data_count}>{ratedSeries?.total_results}</span>
+                    <div className={styles.data_container}>
+                        <div className={styles.data_item}>
+                            <span className={styles.data_label}>Peliculas</span>
+                            <span className={styles.data_count}>{ratedMovies?.total_results}</span>
+                        </div>
+                        <div className={styles.data_item}>
+                            <span className={styles.data_label}>Series de TV</span>
+                            <span className={styles.data_count}>{ratedSeries?.total_results}</span>
+                        </div>
                     </div>
                 </div>
                 <div className={styles.data_section}>
                     <div className={styles.data_item}>
-                        <Link className={styles.data_link} href={" /lists?page=1"}>
+                        <Link className={styles.data_link} href={"/lists?page=1"}>
                             <span className={styles.data_title}>
-                                <ListIcon /> Mis Listas
+                                <ListIcon className={styles.data_logo} /> Mis Listas
                             </span>
                             <ArrowRightIcon className={styles.data_icon} />
                         </Link>
-                        <label className={styles.data_label}>Total de listas</label>
-                        <span className={styles.data_count}>{listMovies?.total_results}</span>
+                    </div>
+                    <div className={styles.data_container}>
+                        <div className={styles.data_item}>
+                            <span className={styles.data_label}>Total de listas</span>
+                            <span className={styles.data_count}>{listMovies?.total_results}</span>
+                        </div>
                     </div>
                 </div>
             </div>
